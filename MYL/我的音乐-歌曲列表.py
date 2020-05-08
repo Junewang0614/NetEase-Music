f2d21a372276bcfcce762658f39f4b7724f926c5
dekuaa <1657720567@qq.com>
2020-05-08 20:24:07 +0800

    logs

--- MYL/我的音乐-歌曲列表.py
+++ MYL/我的音乐-歌曲列表.py
@@ -50,28 +50,28 @@ class WangYiYun(object):
 		self.driver.switch_to.frame(iframe_element)
 
 		names = []
-		with open("我喜欢的音乐.csv", "w", newline='') as f:
+		with open("我喜欢的音乐.csv", "w", newline='', encoding='utf-8') as f:
 			csv_writer = csv.writer(f)
 			csv_writer.writerow(['歌名', '歌手'])
 			try:
-				for i in range(1, 140):
+				for i in range(1, 1000):
 					# 找歌名
 					songs = self.driver.find_element_by_xpath(str.format('//tbody/tr[{0}]/td[2]', i))
 					song_name = songs.find_element_by_css_selector('a[href^="/song?id="]').text
 					song_name = song_name + '\n'
 					song_name = cleanSongname(song_name)
-					# print("歌名:" + song_name)
+					print("歌名:" + song_name)
 					# 找作者
 					hunmen = self.driver.find_element_by_xpath(str.format('//tbody/tr[{0}]/td[4]', i))
 					hunmen_name = hunmen.find_element_by_css_selector('a[class=""]').text
 					hunmen_name = hunmen_name + '\n'
 					hunmen_name = cleanHunmanname(hunmen_name)
-					# print("作者:"+hunmen_name)
+					print("作者:"+hunmen_name)
 					self.names.append(hunmen_name)
 					# 写入文件
 					csv_writer.writerow([song_name, hunmen_name])
 			except:
-				pass
+				print("断掉了!")
 		f.close()
 		self.driver.close()
 
@@ -80,12 +80,12 @@ class WangYiYun(object):
 			if self.names.count(name) > 0:
 				self.count[name] = self.names.count(name)
 		self.count = sorted(self.count.items(), key=lambda x: x[1], reverse=True)
-		print(self.count)
+		#print(self.count)
 		x = []
 		y = []
 		temp1 = 0
 		temp2 = 0
-		f1 = open("歌手.txt", "w")
+		f1 = open("歌手.txt", "w", encoding='utf-8')
 		if len(self.count) > 10:
 			for i in self.count:
 				if temp1 < 10:
@@ -95,7 +95,7 @@ class WangYiYun(object):
 					temp2 += i[1]
 				temp1 += 1
 				f1.write(i[0])
-				f1.wirte("\n")
+				f1.write("\n")
 			x.append("其他")
 			y.append(temp2)
 		else:
@@ -105,8 +105,8 @@ class WangYiYun(object):
 				f1.write(i[0])
 				f1.write("\n")
 
-		print(x)
-		print(y)
+		#print(x)
+		#print(y)
 
 		# 柱状图
 		plt.figure()
@@ -119,7 +119,7 @@ class WangYiYun(object):
 		plt.show()
 
 	def WC(self):
-		with open("歌手.txt") as fp:
+		with open("歌手.txt",encoding='utf-8') as fp:
 			text = fp.read()
 			print(text)
 			font = r'C:\Windows\Fonts\simfang.ttf'
