dec9236e9e840279057105b8e62175d453abc04b
Junewang0614 <1554717543@qq.com>
2020-05-10 12:08:24 +0800

    完成稿

---
+++ WYD/main.py
@@ -0,0 +1,21 @@
+import new_get as ng#用于爬虫
+import print_img as pi #用于生成数据表
+import word_could as wc #用于生成词云
+
+#数据获取
+def main():
+	'''爬虫爬取内容'''
+	data = ng.get_infor()
+	data.tags.reset()
+	'''生成图表'''
+	pi.mixprint3(data.tags.dic,data.tags.dic0)
+	pi.mixprint4(data.tags.dic,data.tags.dic0)
+	'''生成词云'''
+	wc.makeclouds()
+
+if __name__ == '__main__':
+	main()
+
+
+
+
